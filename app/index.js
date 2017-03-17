4cf9ecd836c182eb3ccf73b418dde327a5eab2fe
solominh <hoangminhftu2@gmail.com>
2017-03-18 04:23:52 +0700

    Setup react-devtools-extension

--- app/index.js
+++ app/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+
+import ReduxThunk from 'redux-thunk';
 import ReduxPromise from 'redux-promise';
+import ReduxLogger from 'redux-logger';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(
-  reducers,
-  applyMiddleware(ReduxPromise)
-);
+const middlewares = [ReduxThunk, ReduxPromise];
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(...middlewares),
+  // other store enhancers if any
+));
 
 ReactDOM.render(
   <Provider store={store}>
