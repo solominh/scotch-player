770d4778ec2924b180fadca87da75c0580efd6ce
solominh <hoangminhftu2@gmail.com>
2017-03-18 05:29:19 +0700

    Add debounce when search for songs

--- app/containers/Search.js
+++ app/containers/Search.js
@@ -2,6 +2,7 @@ import Search from '../components/Search';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { search, selectSong } from '../actions/index';
+import _ from 'lodash';
 
 function mapStateToProps({ searchedTracks }) {
   return {
@@ -10,12 +11,20 @@ function mapStateToProps({ searchedTracks }) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators(
-    {
-      search,
-      selectSong,
-    },
-    dispatch);
+  return {
+    search: _.debounce((...args) => {
+      dispatch(search(...args));
+    }, 300),
+    selectSong: (...args) => {
+      dispatch(selectSong(...args));
+    }
+  }
+  // return bindActionCreators(
+  //   {
+  //     search,
+  //     selectSong,
+  //   },
+  //   dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Search);
